a093cf43aafc9412662fcab020b64fafe03e02d5
April Damaso <april@mixedape.fm>
2017-03-15 03:18:10 -0700

    completed noteworthy section

--- js/script.js
+++ js/script.js
@@ -2,28 +2,7 @@ $(document).ready(function(){
 	// When document is ready, fade in webpage
     $('.fade').hide(0).delay(500).fadeIn(3000)
 
-    // Show modal when entry point is clicked
-    $('.donation').click(function() {
-    	$('.overlay').removeClass('hide').addClass('show');
-    	$('.modal').addClass('show');
-    });
-
-    // Close modal when exit points are clicked, reset to begin screen
-    $('.exit').click(function() {
-    	$('.overlay').removeClass('show').addClass('hide');
-    	$('.begin').removeClass('hide').addClass('show');
-    	$('.canadian').removeClass('show').addClass('hide');
-    	$('.global').removeClass('show').addClass('hide');
-    });
-
-    // Close modal when clicked outside
-    $('.overlay').click(function(){
-	     $('.overlay, .modal').removeClass('show').addClass('hide');
-	})                                            
-	$('.modal').click(function(e){
-	   e.stopPropagation();
-	})   
-
+    // Smooth scroll to section when link is clicked
     $(function() {
       $('a[href*="#"]:not([href="#"])').click(function() {
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
