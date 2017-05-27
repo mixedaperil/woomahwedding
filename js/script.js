6a800c3a6716d292e5127a1212594fb4b4620d0f
April Damaso <april@mixedape.fm>
2017-05-27 13:53:32 -0700

    New background, basic mobile navigation js interactions

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 	// When document is ready, fade in webpage
-    $('.fade').hide(0).delay(500).fadeIn(3000)
+    $('.fade').hide(0).delay(200).fadeIn(3500)
+
 
     // Smooth scroll to section when link is clicked
     $(function() {
@@ -18,20 +19,41 @@ $(document).ready(function(){
       });
     });
 
-    // To slide something leftwards into view,
-    // with a delay of 1000 msec
+
+    // Slide in mobile navigation
     $(".mobile-nav").click(function () {
-          $(".mobile-nav-menu").toggle( "slide" );
+      $(".mobile-nav i").removeClass('ion-navicon').addClass('ion-ios-close-empty');
+      $(".mobile-nav-menu").toggle( "slide" );
     });
 
-    // Fade in navigation bar when scrolled to a point
+
+    // Close mobile navigation when clicked outside of container
+    $(document).mouseup(function(e) 
+    {
+      var container = $(".mobile-nav-menu");
+      // If the target of the click isn't the container nor a descendant of the container
+      if (!container.is(e.target) && container.has(e.target).length === 0) {
+        container.hide( "slide" );
+        $(".mobile-nav i").addClass('ion-navicon').removeClass('ion-ios-close-empty');
+      }
+    });
+
+
+    // Close mobile navigation when a menu item is clicked
+    $(".mobile-nav-menu li a").click(function() {
+      $(".mobile-nav-menu").hide( "slide" );
+      $(".mobile-nav i").addClass('ion-navicon').removeClass('ion-ios-close-empty');
+    });
+
+
+    // Fade in desktop navigation bar when scrolled to a point
     window.addEventListener("scroll", function() {
-        if (window.scrollY > 720) {
-            $('#menu').addClass('fixed-nav').fadeIn();
-        }
-        else {
-            $('#menu').removeClass('fixed-nav');
-        }
+      if (window.scrollY > 720) {
+        $('#menu').addClass('fixed-nav').fadeIn();
+      }
+      else {
+        $('#menu').removeClass('fixed-nav');
+      }
     });
 
 
